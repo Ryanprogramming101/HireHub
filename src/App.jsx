a53a80e205f1a9e0f45fbc18ef0bdfa4104f821a
Ryan <ryanprogramming101@gmail.com>
2024-06-07 00:15:33 +0800

    react-router-dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import {
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import PageNotFound from './pages/PageNotFound';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<MainLayout />}>
+			<Route index element={<HomePage />} />
+			<Route path='/jobs' element={<JobsPage />} />
+			<Route path='*' element={<PageNotFound />} />
+		</Route>
+	)
+);
 
 const App = () => {
-	return (
-		<>
-			<Navbar />
-			<Hero />
-			<HomeCards />
-			<JobListings />
-			<ViewAllJobs />
-		</>
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default App;
