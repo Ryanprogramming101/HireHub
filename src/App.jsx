edba553e78de52f29c3f7b863e5896371278da84
Ryan <ryanprogramming101@gmail.com>
2024-06-06 17:59:00 +0800

    Sorted components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
-	const names = ['Ryan', 'Sam', 'Cass', 'Someone'];
-	const loggedIn = true;
-	const styles = {
-		color: 'red',
-		fontSize: '36px',
-	};
-
 	return (
 		<>
-			<div className='text-5xl'>App</div>
-			<p style={styles}>Testing object styling</p>
-			<p style={{ color: 'red', fontSize: '24px' }}>Testing inline style</p>
-			<ul>
-				{names.map((name, index) => (
-					<li key={index}>{name}</li>
-				))}
-			</ul>
-			{loggedIn ? <h1>Hello {names[0]}! </h1> : <h1>Hello Guest!</h1>}
+			<Navbar />
+			<Hero />
+			<HomeCards />
+			<JobListings />
+			<ViewAllJobs />
 		</>
 	);
 };
