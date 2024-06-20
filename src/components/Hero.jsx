cc736d92fdb3ec9b574a1f88afc48116500103eb
Ryan <ryanprogramming101@gmail.com>
2024-06-20 22:17:38 +0800

    Changed Hire Hub to HireHub and updated jobs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
 			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
 				<div className='text-center'>
 					<h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>
-						Hire Hub
+						HireHub
 					</h1>
 					<p className='my-4 text-xl text-white'>
 						Find your dream job that fits your skills and needs with just
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 						>
 							<img className='h-10 w-auto' src={logo} alt='React Jobs' />
 							<span className='hidden md:block text-white text-2xl font-bold ml-2'>
-								Hire Hub
+								HireHub
 							</span>
 						</NavLink>
 						<div className='md:ml-auto'>
